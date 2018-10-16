2ad0758d950eadd2aad8bb8c6676aec42eb5b583
misterxretsim <mister4xxxx@bk.ru>
2018-10-16 15:33:57 +0300

    2nd commit

--- front/script.js
+++ front/script.js
@@ -1,6 +1,52 @@
 $('document').ready(function(){
 
 	var flagForSettings = false;
+	var flagForTimezones = false;
+	var valueForTimezone = 'cool';
+	var windowHeight = window.innerHeight;
+	var height = ((window.innerHeight / 100) * 139.8) + 'px';
+	var zonesArr = ['GMT Среднее время по Гринвичу','UTC Всемирное координированное время','WET Западноевропейское время','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''];
+
+	for(var i = 0; i < 43; i++){
+
+		$('<div/>', {
+
+			class: 'timezones',
+			id: 'zone' + i,
+		}).appendTo('#divForTimezones');
+		document.getElementById('zone' + i).innerHTML += zonesArr[i];
+	}
+
+	$('#timezone').on('click', function(){
+		
+		if(flagForTimezones == false){
+
+			$('#divForTimezones').css('display', 'block');
+			$('#supportDivForBottomBorder').css('display', 'block');
+			$('body').css('height', height);
+			flagForTimezones = true;
+		}else{
+
+			$('#divForTimezones').css('display', 'none');
+			$('#supportDivForBottomBorder').css('display', 'none');
+			$('body').css('height', windowHeight);
+			flagForTimezones = false;
+		}
+	});
+
+	$('.timezones').on('click', function(){
+
+		var id = $(this).attr('id');
+		id = id.replace('zone', '');
+		$('#timezone').attr('value', zonesArr[id]);
+		if(flagForTimezones == true){
+
+			$('#divForTimezones').css('display', 'none');
+			$('#supportDivForBottomBorder').css('display', 'none');
+			$('body').css('height', windowHeight);
+			flagForTimezones = false;
+		}
+	});
 
 	$('.fa-cog').on('click', function(){
 		if(flagForSettings == false){
@@ -20,6 +66,17 @@ $('document').ready(function(){
 			$('#wrapperForSettings').css('display', 'none');	
 			flagForSettings = false;	
 		}
+		if(flagForTimezones == true){
+
+			$('#divForTimezones').css('display', 'none');
+			$('#supportDivForBottomBorder').css('display', 'none');
+			$('body').css('height', windowHeight);
+			flagForTimezones = false;
+		}
+	});
+	$('#qestionForTimezone').on('click', function(){
+		
+		$('#timezone').attr('value', valueForTimezone);
 	});
 
 
