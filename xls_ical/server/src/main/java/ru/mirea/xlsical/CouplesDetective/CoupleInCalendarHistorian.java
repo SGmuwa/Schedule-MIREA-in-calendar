9992dfa2a181dcb63e1f834951bd6b7872ab3ec3
SG_Muwa <motherlode.muwa@gmail.com>
2018-11-13 10:07:34 +0300

    Update xls_ical/server/src/main/java/ru/mirea/xlsical/CouplesDetective/CoupleInCalendarHistorian.java

---
+++ xls_ical/server/src/main/java/ru/mirea/xlsical/CouplesDetective/CoupleInCalendarHistorian.java
@@ -0,0 +1,8 @@
+package ru.mirea.xlsical.CouplesDetective;
+
+/**
+ * Класс реализует защиту от записи прошедших пар.
+ * Также предсказывает, какие пары будут в будущем.
+ */
+public class CoupleInCalendarHistorian {
+}
