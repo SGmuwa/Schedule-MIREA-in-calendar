6136f126caf5c316f630c4906edd3649a9de2417
Artemy <slacky@ya.ru>
2018-12-06 20:56:28 +0300

    взято из

--- xls_ical/backend_1/src/main/java/ru/mirea/xlsical/backend/utils/TimezoneDisplayApp.java
+++ xls_ical/backend_1/src/main/java/ru/mirea/xlsical/backend/utils/TimezoneDisplayApp.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 
-// взято из
+// взято из https://www.baeldung.com/java-time-zones
 public class TimezoneDisplayApp {
 
     public enum OffsetBase {
