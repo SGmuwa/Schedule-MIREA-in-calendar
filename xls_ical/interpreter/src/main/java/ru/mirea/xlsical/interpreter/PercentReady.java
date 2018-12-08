b712397f302ddf449b355976c3c9523512c9e708
SG_Muwa <motherlode.muwa@gmail.com>
2018-12-08 08:22:38 +0300

    PercentReady update
    Появилась поддержка вложенности. Поддержка отправки отчёта.

--- xls_ical/interpreter/src/main/java/ru/mirea/xlsical/interpreter/PercentReady.java
+++ xls_ical/interpreter/src/main/java/ru/mirea/xlsical/interpreter/PercentReady.java
@@ -3,7 +3,7 @@ package ru.mirea.xlsical.interpreter;
 /**
  * Класс, который служит для управления процентом готовности задачи.
  * @since 18.11.2018
- * @version 18.11.2018
+ * @version 2018-12-08
  * @author <a href="https://github.com/SGmuwa/">[SG]Muwa</a>
  */
 public class PercentReady {
@@ -12,19 +12,46 @@ public class PercentReady {
      * Создание нового экземпляра управления процентом готовности.
      */
     public PercentReady(){
-        whole = null;
-        coefficient = 1.0f;
+        this(null, 1.0f);
+    }
+
+    /**
+     * Создание нового экземпляра управления процентом готовности.
+     * @param subscriber Функция, которую надо вызывать при каждом изменении значения.
+     */
+    public PercentReady(ICanUsePercentReady subscriber) {
+        this(null, 1.0f, subscriber);
+    }
+
+    /**
+     * Создание потомка, который изменяет общий сумматор.
+     * @param whole Ссылка на целую часть.
+     * @param coefficient Коэфициент взаимодействя на целую часть. Допустимые значения: от 0 до 1.
+     */
+    public PercentReady(PercentReady whole, float coefficient) {
+        this(whole, coefficient, null);
     }
 
     /**
      * Создание ребёнка, который изменяет общий сумматор.
      * @param whole Ссылка на целую часть.
      * @param coefficient Коэфициент взаимодействя на целую часть. Допустимые значения: от 0 до 1.
+     * @param subscriber Функция, которую надо вызывать при каждом изменении значения.
      */
-    public PercentReady(PercentReady whole, float coefficient) {
+    public PercentReady(PercentReady whole, float coefficient, ICanUsePercentReady subscriber) {
         this.whole = whole;
-        if(0.0f <= coefficient && coefficient <= 1.0f)
+        this.subscriber = subscriber;
+        if(0.0f <= coefficient && coefficient <= 1.0f) {
             this.coefficient = coefficient;
+            if(whole != null) {
+                synchronized (whole.sc) {
+                    if (whole.sumOfCoefficientInPieces + coefficient > 1.0f)
+                        throw new IllegalArgumentException("Сумма коэффициентов целой части зашкаливает! Должно быть от 0 до 1 включительно! А получается: " + whole.sumOfCoefficientInPieces + coefficient);
+                    else
+                        whole.sumOfCoefficientInPieces += coefficient;
+                }
+            }
+        }
         else
             throw new IllegalArgumentException("Коэффициент должен быть от 0 до 1 включительно!");
     }
@@ -45,6 +72,14 @@ public class PercentReady {
      * Коэффициент.
      */
     public final float coefficient;
+    /**
+     * Подписчик на изменения.
+     */
+    private final ICanUsePercentReady subscriber;
+    /**
+     * Сумма коэфициентов в частях.
+     */
+    private float sumOfCoefficientInPieces = 0.0f;
 
     /**
      * Возвращает процент готовности от 0 до 1.
@@ -61,17 +96,68 @@ public class PercentReady {
      * @param ready Процент готовности от 0 до 1.
      */
     public void setReady(float ready) {
-        synchronized (sc) {
-            if (whole == null)
-                if (0.0f <= ready && ready <= 1.0f)
+        IllegalArgumentException error = null;
+        synchronized (sc) { // Блокировка этого состояния.
+            if (0.0f <= ready && ready <= 1.0f) {
+                if (whole == null)
                     this.ready = ready;
-                else
-                    throw new IllegalArgumentException("float ready can be only 0.0f ... 1.0f!");
-            else {
-                synchronized (whole.sc) {
-
+                else {
+                    error = this.sendToWhole(ready);
                 }
+            } else
+                error = new IllegalArgumentException("float ready can be only 0.0f ... 1.0f! Argument = " + ready);
+        }
+        if (error != null)
+            throw error;
+        if(whole == null)
+            if(this.subscriber != null)
+                this.subscriber.transferValue(this);
+    }
+
+    private IllegalArgumentException sendToWhole(float ready) {
+        IllegalArgumentException error = null;
+        if(whole != null) {
+            synchronized (whole.sc) { // Блокировка целой части.
+                // Восстановление значения.
+                float wholeWithoutThis = whole.ready - (this.ready * coefficient);
+                this.ready = ready;
+                float commit = wholeWithoutThis + (this.ready * coefficient);
+                if (-0.1f <= commit && commit <= 1.1f) {
+                    if (commit > 1.0f)
+                        commit = 1.0f;
+                    else if (commit < 0.0f)
+                        commit = 0.0f;
+                    error = whole.sendToWhole(commit);
+                } else
+                    error = new IllegalArgumentException("I can't send new value: " + commit);
             }
         }
+        else { // Это корень! Это и есть сумма всех частей.
+            // synchronized sc // уже заблокировано.
+            if (0.0f <= ready && ready <= 1.0f)
+                this.ready = ready;
+            else
+                error = new IllegalArgumentException("While i go through wholes: float ready can be only 0.0f ... 1.0f! Argument = " + ready);
+        }
+        if(error == null)
+            if(subscriber != null)
+                subscriber.transferValue(this);
+        return error;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%3.0f%%", ready*100f);
+    }
+}
+
+/**
+ * Интерфейс говорит, что объект умеет принимать изменения percentReady
+ */
+interface ICanUsePercentReady {
+    /**
+     * Вызывается всегда, когда используется setValue.
+     * @param percentReady Объект, который был изменён.
+     */
+    void transferValue(PercentReady percentReady);
 }
---
+++ xls_ical/interpreter/src/test/java/ru/mirea/xlsical/interpreter/PercentReadyTest.java
@@ -0,0 +1,81 @@
+package ru.mirea.xlsical.interpreter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PercentReadyTest {
+
+    @Test
+    public void startSimplePercent() {
+        PercentReady pr = new PercentReady();
+        assertEquals(0.0f, pr.getReady(), 0f);
+        pr.setReady(0.5f);
+        assertEquals(0.5f, pr.getReady(), 0f);
+        try {
+            pr.setReady(2f);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals(0.5f, pr.getReady(), 0f);
+        }
+        pr.setReady(1.0f);
+        assertEquals(1.0f, pr.getReady(), 0.0f);
+    }
+
+    @Test
+    public void startWholePercent() throws InterruptedException {
+        PercentReady whole = new PercentReady(new ICanUsePercentReady() {
+            String old = "";
+
+            @Override
+            public void transferValue(PercentReady pr) {
+                String newStr = pr.toString();
+                if(!newStr.equals(old)) {
+                    old = newStr;
+                    System.out.println(newStr);
+                }
+            }
+        });
+        Thread a = new Thread(() -> function1(new PercentReady(whole, 1f/11f)));
+        Thread b = new Thread(() -> function2(new PercentReady(whole, 10f/11f)));
+        a.start();
+        b.start();
+        System.out.println("wait a.");
+        a.join();
+        System.out.println("a ready, wait b.");
+        b.join();
+        System.out.println("b ready");
+    }
+
+    private void function1(PercentReady pr) {
+        pr.setReady(0.0f);
+        for(int i = 0; i < 10000; i++) {
+            pr.setReady((float)i/9999.0f);
+        }
+    }
+
+    private void function2(PercentReady pr) {
+        pr.setReady(0.0f);
+        for(int i = 0; i < 100000; i++) {
+            pr.setReady((float)i/99999.0f);
+        }
+    }
+
+    @Test
+    public void testSimpleFail() {
+        try {
+            new PercentReady().setReady(2);
+            fail();
+        } catch (IllegalArgumentException e) {/*good!*/}
+        try {
+            new PercentReady(null, 2.0f);
+            fail();
+        } catch (IllegalArgumentException e) {/*good!*/}
+        try {
+            PercentReady big = new PercentReady();
+            new PercentReady(big, 0.5f);
+            new PercentReady(big, 0.6f);
+            fail();
+        } catch (IllegalArgumentException e) {/*good!*/}
+    }
+}
