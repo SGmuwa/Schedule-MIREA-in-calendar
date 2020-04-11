e3ce4f49e79a37bca9822822d5ab58166dd21a51
SG_Muwa <motherlode.muwa@gmail.com>
2020-04-12 02:44:05 +0300

    Rewrite "BinarySearch" to C#

--- xls_ical/ICalMaker/src/main/java/ru/mirea/xlsical/CouplesDetective/BinarySearch.cs
+++ xls_ical/ICalMaker/src/main/java/ru/mirea/xlsical/CouplesDetective/BinarySearch.cs
@@ -1,10 +1,10 @@
-package ru.mirea.xlsical.CouplesDetective;
+using System.Collections.Generic;
+using System;
 
-
-import java.util.*;
-
+namespace ru.mirea.xlsical.CouplesDetective
+{
 public class BinarySearch {
-    private static <T> int BinarySearch_Iter(ICanGetValueByIndex<T> array, int size, boolean descendingOrder, T key, Comparator<T> comparator) {
+    private static int BinarySearch_Iter<T>(IReadOnlyList<T> array, int size, bool descendingOrder, T key, IComparer<T> comparator) {
         int left = 0;
         int right = size;
         int mid = 0;
@@ -13,10 +13,10 @@ public class BinarySearch {
         {
             mid = left + (right - left) / 2;
 
-            if (array.get(mid) == key)
+            if (array[mid].Equals(key))
                 return mid;
 
-            if ((comparator.compare(array.get(mid),key) > 0) ^ descendingOrder)
+            if ((comparator.Compare(array[mid],key) > 0) ^ descendingOrder)
                 right = mid;
             else
                 left = mid + 1;
@@ -29,37 +29,37 @@ public class BinarySearch {
      * Бинарный поиск индекса элемента. Укажет индекс самого ближнего к индексу 0 соответствующего элемента. Если не нашёл - показывает ближайший индекс под оператором ~.
      * @param array Входящий массив, в котором надо искать. Массив должен быть отсортирован.
      * @param key То, что надо найти
-     * @param <E> Тим должен уметь стравниваться.
+     * @param <E> Тим должен уметь сравниваться.
      * @return Индекс элемента. Если не найден, то ищется ближайший элемент, который меньше {@code key},
-     * а затем результат ивенсируется с помощью оператора {@code ~}. Если все элементы массива больше {@code key},
+     * а затем результат инвертируется с помощью оператора {@code ~}. Если все элементы массива больше {@code key},
      * то возвращается {@link Integer#MIN_VALUE}.
      */
-    public static <E extends Comparable<? super E>> int BinarySearch_Iter_Wrapper(List<E> array, E key)
+    public static int BinarySearch_Iter_Wrapper<E>(List<E> array, E key) where E : IComparable<E>
     {
         if(array == null)
-            throw new NullPointerException("array must be not null!");
-        if (array.size() == 0 || key == null)
-            return Integer.MIN_VALUE;
-        int left = BinarySearch_Iter(array::get, array.size(), array.get(0).compareTo(array.get(array.size() - 1)) > 0, key, (a, b) -> a.compareTo(b));
+            throw new ArgumentNullException("array must be not null!");
+        if (array.Count == 0 || key == null)
+            return int.MinValue;
+        int left = BinarySearch_Iter(array, array.Count, array[0].CompareTo(array[array.Count - 1]) > 0, key, (a, b) -> a.compareTo(b));
         if(left < 0)
             left = ~left;
-        if(left >= array.size())
-            left = array.size() - 1;
-        while(left >= 0 && array.get(left).compareTo(key) >= 0)
+        if(left >= array.Count)
+            left = array.Count - 1;
+        while(left >= 0 && array[left].CompareTo(key) >= 0)
             left--;
-        if(left + 1 < array.size() && array.get(left + 1).compareTo(key) == 0)
+        if(left + 1 < array.Count && array[left + 1].CompareTo(key) == 0)
             left++;
         if(left == -1) {
-            if (array.get(0).compareTo(key) == 0)
+            if (array[0].compareTo(key) == 0)
                 return 0;
-            return Integer.MIN_VALUE;
+            return int.MinValue;
         }
         return array.get(left).compareTo(key) == 0 ? left : ~left;
     }
 
-    public static <E> int BinarySearch_Iter_Wrapper(E key, ICanGetValueByIndex<E> array, int size, Comparator<? super E> comparator) {
+    public static int BinarySearch_Iter_Wrapper<E>(E key, ICanGetValueByIndex<E> array, int size, IComparer<E> comparator) {
         if(array == null || comparator == null)
-            throw new NullPointerException("array must be not null!");
+            throw new ArgumentNullException("array must be not null!");
         if (size == 0 || key == null)
             return Integer.MIN_VALUE;
         int left = BinarySearch_Iter(array::get, size, comparator.compare(array.get(0), array.get(size - 1)) > 0, key, comparator);
@@ -81,5 +81,6 @@ public class BinarySearch {
 }
 
 interface ICanGetValueByIndex<E> {
-    E get(int index);
+    E this[int index] { get; }
+}
 }
