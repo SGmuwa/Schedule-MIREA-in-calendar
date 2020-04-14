48a8d0e3b01b2539363632ac28d9d63dbb2b0b91
SG_Muwa <motherlode.muwa@gmail.com>
2020-04-14 07:26:40 +0300

    ICalMaker: Couple.cs to C#

--- ICalMaker/src/main/java/ru/mirea/xlsical/CouplesDetective/Couple.cs
+++ ICalMaker/src/main/java/ru/mirea/xlsical/CouplesDetective/Couple.cs
@@ -16,79 +16,83 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-package ru.mirea.xlsical.CouplesDetective;
+using System;
 
-import java.io.Serializable;
-import java.util.Objects;
-
-/**
- * Класс, который описывает в общем, что общего между.
- * календарной и эксель парой.
- * @since 16.11.2018
- * @version 16.11.2018
- * @author <a href="https://github.com/SGmuwa/">[SG]Muwa</a>
- * @see CoupleInCalendar Пара для календаря
- */
-public class Couple implements Serializable {
+namespace ru.mirea.xlsical.CouplesDetective
+{
 
     /**
-     * Название пары.
+     * Класс, который описывает в общем, что общего между.
+     * календарной и эксель парой.
+     * @since 16.11.2018
+     * @version 16.11.2018
+     * @author <a href="https://github.com/SGmuwa/">[SG]Muwa</a>
+     * @see CoupleInCalendar Пара для календаря
      */
-    public final String itemTitle;
-    /**
-     * Тип занятия (лекция, практика, лабораторная работа)
-     */
-    public final String typeOfLesson;
-    /**
-     * Название группы.
-     */
-    public String nameOfGroup;
-    /**
-     * Имя преподавателя.
-     */
-    public String nameOfTeacher;
-    /**
-     * Номер аудитории.
-     */
-    public final String audience;
-    /**
-     * Адрес корпуса.
-     */
-    public final String address;
+    public class Couple
+    {
 
-    /**
-     * Создание в памяти экземпляров параметров вне-временых параметров пары.
-     * @param itemTitle Название пары.
-     * @param typeOfLesson Тип занятия (лекция, практика, лабораторная работа)
-     * @param nameOfGroup Название группы.
-     * @param nameOfTeacher Имя преподавателя.
-     * @param audience Номер аудитории.
-     * @param address Адрес корпуса.
-     */
-    protected Couple(String itemTitle, String typeOfLesson, String nameOfGroup, String nameOfTeacher, String audience, String address) {
-        this.itemTitle = itemTitle;
-        this.typeOfLesson = typeOfLesson;
-        this.nameOfGroup = nameOfGroup;
-        this.nameOfTeacher = nameOfTeacher;
-        this.audience = audience;
-        this.address = address;
-    }
+        /**
+         * Название пары.
+         */
+        public string ItemTitle { get; }
+        /**
+         * Тип занятия (лекция, практика, лабораторная работа)
+         */
+        public string typeOfLesson { get; }
+        /**
+         * Название группы.
+         */
+        public string NameOfGroup { get; }
+        /**
+         * Имя преподавателя.
+         */
+        public string NameOfTeacher { get; set; }
+        /**
+         * Номер аудитории.
+         */
+        public string Audience { get; set; }
+        /**
+         * Адрес корпуса.
+         */
+        public string Address { get; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Couple)) return false;
-        Couple couple = (Couple) o;
-        return Objects.equals(itemTitle, couple.itemTitle) &&
-                Objects.equals(typeOfLesson, couple.typeOfLesson) &&
-                Objects.equals(nameOfGroup, couple.nameOfGroup) &&
-                Objects.equals(nameOfTeacher, couple.nameOfTeacher) &&
-                Objects.equals(audience, couple.audience) &&
-                Objects.equals(address, couple.address);
-    }
+        /**
+         * Создание в памяти экземпляров параметров вне-временных параметров пары.
+         * @param itemTitle Название пары.
+         * @param typeOfLesson Тип занятия (лекция, практика, лабораторная работа)
+         * @param nameOfGroup Название группы.
+         * @param nameOfTeacher Имя преподавателя.
+         * @param audience Номер аудитории.
+         * @param address Адрес корпуса.
+         */
+        protected Couple(string itemTitle, string typeOfLesson, string nameOfGroup, string nameOfTeacher, string audience, string address)
+        {
+            this.ItemTitle = itemTitle;
+            this.typeOfLesson = typeOfLesson;
+            this.NameOfGroup = nameOfGroup;
+            this.NameOfTeacher = nameOfTeacher;
+            this.Audience = audience;
+            this.Address = address;
+        }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(itemTitle, typeOfLesson, nameOfGroup, nameOfTeacher, audience, address);
+        public override bool Equals(object o)
+        {
+            if (o == null) return false;
+            if (this == o) return true;
+            if (o is Couple couple)
+            {
+                return object.Equals(ItemTitle, couple.ItemTitle) &&
+                        object.Equals(typeOfLesson, couple.typeOfLesson) &&
+                        object.Equals(NameOfGroup, couple.NameOfGroup) &&
+                        object.Equals(NameOfTeacher, couple.NameOfTeacher) &&
+                        object.Equals(Audience, couple.Audience) &&
+                        object.Equals(Address, couple.Address);
+            }
+            else return false;
+        }
+
+        public override int GetHashCode()
+            => HashCode.Combine(ItemTitle, typeOfLesson, NameOfGroup, NameOfTeacher, Audience, Address);
     }
 }
